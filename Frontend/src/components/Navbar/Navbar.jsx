ae0cc63bc8a66267b3929f3c00f35a3c121d3b3b
Sharad072 <sharad.gumbar@capx.global>
2024-06-14 19:06:44 +0530

    updated

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
 import './Navbar.css'
+import wp from './wplogo.webp'
+import tg from './tglogo.png'
 
 export default function Navbar() {
   const[menu, setMenu] = useState("Home");
 
+  const handleWhatsApp = () =>{
+    window.open('https://wa.me/919837290556? text=Hello How are you?', '_blank' )
+  }
+
+  const handleTelegramApp = () =>{
+    window.open('https://telegram.me/KeshavBajaj04? text=Hello How are you?', '_blank' )
+  }
   return (
     <div className='navbar'>
       <div className="left">
@@ -13,6 +22,13 @@ export default function Navbar() {
           <li onClick={() => setMenu("Services")} className={menu==="Services"?"active":""}>Services</li>
           <li onClick={() => setMenu("Membership")} className={menu==="Membership"?"active":""}>Membership</li>
        </ul>
+       </div>
+       <div className="contacts">
+       <div className="wp">
+
+            <img src={wp} alt="aaa" className='contact-wp-img' onClick={handleWhatsApp}/>
+            <img src={tg} alt="aaa" className='contact-tg-img' onClick={handleTelegramApp} />
+       </div>
        </div>
     </div>
   )
