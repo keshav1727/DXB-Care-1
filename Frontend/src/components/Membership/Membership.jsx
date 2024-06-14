97757c51bffbf7ee7500e8eb2a92a18a60f7ad84
Sharad072 <sharad.gumbar@capx.global>
2024-06-14 15:05:46 +0530

    added new section

--- Frontend/src/components/Membership/Membership.jsx
+++ Frontend/src/components/Membership/Membership.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import './Membership.css'
 
 export default function Membership() {
   return (
     <div className='membership'>
+        <div className="content">
         <div className='header'><h1 className='heading'>Membership</h1></div>
         <div className="membership-content">
-            <div className="item"></div>
-            <div className="item"></div>
-            <div className="item"></div>
+            <div className="item">
+              <div className='item-name'> <h1 className='item-heading'>Corporate</h1> </div>
+              <div className='item-content'><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio quasi impedit perferendis eveniet maiores saepe esse repellat aliquid enim voluptas. Natus eos nesciunt quam dolor ducimus eaque sint repellat velit quisquam ea, labore blanditiis cum voluptates distinctio voluptas asperiores molestiae quae vero officiis? Est libero rerum maiores quisquam iusto eius deleniti animi, placeat quaerat quam recusandae provident exercitationem omnis blanditiis.</p></div>
+            </div>
+            <div className="item">
+            <div className='item-name'> <h1 className='item-heading'>Personal</h1> </div>
+            <div className='item-content'><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio quasi impedit perferendis eveniet maiores saepe esse repellat aliquid enim voluptas. Natus eos nesciunt quam dolor ducimus eaque sint repellat velit quisquam ea, labore blanditiis cum voluptates distinctio voluptas asperiores molestiae quae vero officiis? Est libero rerum maiores quisquam iusto eius deleniti animi, placeat quaerat quam recusandae provident exercitationem omnis blanditiis.</p></div>
+            </div>
+            <div className="item">
+            <div className='item-name'>  <h1 className='item-heading'>Make your own plan</h1> </div>
+            <div className='item-content'><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio quasi impedit perferendis eveniet maiores saepe esse repellat aliquid enim voluptas. Natus eos nesciunt quam dolor ducimus eaque sint repellat velit quisquam ea, labore blanditiis cum voluptates distinctio voluptas asperiores molestiae quae vero officiis? Est libero rerum maiores quisquam iusto eius deleniti animi, placeat quaerat quam recusandae provident exercitationem omnis blanditiis.</p></div>
+            </div>
+        </div>
         </div>
       
     </div>
