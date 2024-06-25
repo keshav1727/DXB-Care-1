9972f9526de1d9e29e3ea73de957b48ae2ddfe56
Sharad072 <sharad.gumbar@capx.global>
2024-06-25 23:07:13 +0530

    update

--- Frontend/src/components/Membership/Membership.jsx
+++ Frontend/src/components/Membership/Membership.jsx
@@ -28,9 +28,11 @@ export default function Membership() {
         <div className='header'><h1 className='heading'>Membership</h1></div>
         <div className="membership-content">
           <div className="item">
+            <div className="member-image">
             <img src={corp} alt="Corporate" className="item-image" />
+            </div>
             <div className='item-name'><h1 className='item-heading'>Corporate</h1></div>
-            <div className='item-content'><p>Individualized Experience Just for You.</p></div>
+            <div className='item-content'><p>We share a common goal: SUCCESS in our endeavors.</p></div>
             <div className="button"><button onClick={Corporatehandle}>Explore
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-right" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
@@ -38,9 +40,11 @@ export default function Membership() {
             </button></div>
           </div>
           <div className="item">
+            <div className="member-imager">
             <img src={personal} alt="Personal" className="item-image" />
+            </div>
             <div className='item-name'><h1 className='item-heading'>Personal</h1></div>
-            <div className='item-content'><p>Priority bookings for corporate events.</p></div>
+            <div className='item-content'><p>We'll handle it. Just name it, and consider it done.</p></div>
             <div className="button"><button onClick={Personalhandle}>Explore
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-right" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
@@ -48,7 +52,9 @@ export default function Membership() {
             </button></div>
           </div>
           <div className="item">
+            <div className="member-image">
             <img src={relocation} alt="Relocation" className="item-image" />
+            </div>
             <div className='item-name'><h1 className='item-heading'>Relocation</h1></div>
             <div className='item-content'><p>Get oriented with cultural insights and local expertise.</p></div>
             <div className="button"><button onClick={Relocatehandle}>Explore
@@ -58,7 +64,9 @@ export default function Membership() {
             </button></div>
           </div>
           <div className="item">
+            <div className="member-image">
             <img src={visitor} alt="Exclusive Visitors" className="item-image" />
+            </div>
             <div className='item-name'><h1 className='item-heading'>Exclusive Visitors</h1></div>
             <div className='item-content'><p>Receive personalized travel itineraries to make the most of your visit.</p></div>
             <div className="button"><button onClick={Visitorhandle}>Explore
@@ -70,7 +78,7 @@ export default function Membership() {
         </div>
         <div className="crypto-tag">
           <div className="heading">
-            <h1> We also accept <span>Crypto</span> payments</h1>
+            <h1> We also accept <span className='crypto'>Crypto</span> payments</h1>
           </div>
         </div>
       </div>
