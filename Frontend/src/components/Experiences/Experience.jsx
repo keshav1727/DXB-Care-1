721322851b40b6db46d7f125697eb233408833e2
Sharad702 <sharadgumber0702@gmail.com>
2024-06-26 00:08:05 +0530

    update

--- Frontend/src/components/Experiences/Experience.jsx
+++ Frontend/src/components/Experiences/Experience.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Experience.css';
 import aa from './princehonasa_A_real_image_shot_by_a_photographer_of_a_private_j_c954b67a-ce8b-44da-b50c-39e497098a67.png';
-import bb from './princehonasa_Luxury_car_in_dubai_in_day_light_with_dubai_skylin_0a5ed554-7ee1-4ae3-8c95-19be9f034f29.png';
+import bb from './Car f.jpg';
 import cc from './princehonasa_luxury_yatch_in_dubai_in_day_light_with_skyline_du_f6790474-1697-416a-8b92-ce444a5e83a2.png';
 import dd from './11.jpg';
 import { Link, useNavigate } from 'react-router-dom';
